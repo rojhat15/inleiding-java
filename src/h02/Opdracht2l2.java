c4f22cd1399987f2c80a9b7130deebb412e455c1
rojhat <ryildirim@roc-dev.com>
2019-02-15 11:03:02 +0100

    hoofdstuk 2

---
+++ src/h02/Opdracht2l2.java
@@ -0,0 +1,28 @@
+package h02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht2l2 extends Applet {
+
+    public void init() {
+        setBackground(Color.white);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.blue);
+        g.drawString("Hallo Rojhat", 50, 60 );
+        g.setColor(Color.red);
+        g.drawString("Yildirim", 50, 80);
+
+
+      /*  g.drawRect(400,400,300,400); */
+    }
+
+
+
+
+
+}
+
+
